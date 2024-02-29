bb260a3a0ab462ac437f7d3c7898a0d724b2aa63
Dmytro KHRUSHCH <dmytro.khrushch@smile-ukraine.com>
2024-02-29 13:49:17 +0200

    Add mass delete action to entity listing

---
+++ Controller/Adminhtml/Entity/MassDelete.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Smile\CustomEntity\Controller\Adminhtml\Entity;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpPostActionInterface;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Ui\Component\MassAction\Filter;
+use Smile\CustomEntity\Model\ResourceModel\CustomEntity\CollectionFactory;
+
+/**
+ * Mass delete custom entity controller
+ */
+class MassDelete extends Action implements HttpPostActionInterface
+{
+    protected $filter;
+
+    protected $collectionFactory;
+
+    /**
+     * Constructor.
+     *
+     * @param Context $context
+     * @param Filter $filter
+     * @param CollectionFactory $collectionFactory
+     */
+    public function __construct(
+        Context $context,
+        Filter $filter,
+        CollectionFactory $collectionFactory
+    ) {
+        $this->filter = $filter;
+        $this->collectionFactory = $collectionFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function execute()
+    {
+        $collection = $this->filter->getCollection($this->collectionFactory->create());
+        $collectionSize = $collection->getSize();
+
+        foreach ($collection as $entity) {
+            $entity->delete();
+        }
+
+        $this->messageManager->addSuccessMessage(
+            __('A total of %1 record(s) have been deleted.', $collectionSize)
+        );
+
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+
+        return $resultRedirect->setPath('*/*/');
+    }
+}
